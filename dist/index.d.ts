04029f5d393de64ab127f8a52faa2d338fa8e48e
Ben Meyrick <bameyrick@gmail.com>
2025-03-18 10:55:11 +0000

    Update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
 import cogProtocol from './cogProtocol';
+import { getMetadata } from './read/getMetadata';
 import locationValues from './read/locationValues';
 import { colorScale, colorSchemeNames } from './render/colorScale';
 import setColorFunction from './render/custom/setColorFunction';
 import { clearMask, setMask } from './render/masking';
-export { clearMask, cogProtocol, colorScale, colorSchemeNames, locationValues, setColorFunction, setMask };
+export { clearMask, cogProtocol, colorScale, colorSchemeNames, getMetadata, locationValues, setColorFunction, setMask };
